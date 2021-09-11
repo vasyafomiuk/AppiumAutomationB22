81acc282b18b6f27b6710ad7c7e898ca183e5651
Vasyl Fomiuk <vasyl@cybertekschool.com>
2021-09-11 15:59:24 -0400

    Update CalculatorTests.java

--- src/test/java/com/cybertek/automation/tests/CalculatorTests.java
+++ src/test/java/com/cybertek/automation/tests/CalculatorTests.java
@@ -5,6 +5,7 @@ import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
@@ -24,6 +25,16 @@ public class CalculatorTests {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         //for consts: CapabilityType, MobileCapabilityType, AndroidMobileCapabilityType
         //UiAutomator2 - used for android devices automation
+        /**
+         * {
+         *   "platformName": "Android",
+         *   "platformVersion": "7.0",
+         *   "deviceName": "Pixel_2",
+         *   "automationName": "UiAutomator2",
+         *   "appPackage": "com.android.calculator2",
+         *   "appActivity": "com.android.calculator2.Calculator"
+         * }
+         */
         desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
         desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.0");
@@ -37,6 +48,14 @@ public class CalculatorTests {
         // wd/hub - path for Appium server clients same as for Selenium Grid
         URL url = new URL("http://localhost:4723/wd/hub");
         AppiumDriver<MobileElement> driver = new AndroidDriver<>(url, desiredCapabilities);
+        driver.findElementById("com.android.calculator2:id/digit_7").click(); // 7
+        driver.findElementByAccessibilityId("plus").click(); // +
+        driver.findElementById("com.android.calculator2:id/digit_5").click(); // 5
+        driver.findElementByAccessibilityId("equals").click(); // =
+        MobileElement result = driver.findElementById("com.android.calculator2:id/result"); // result = 12
+
+        Assert.assertEquals("12", result.getText());
+
         Thread.sleep(3000);
         driver.closeApp();
     }
