ae13db21d09c2d4dca6a63e6c32f95b4b86081bf
Vasyl Fomiuk <vasyl@cybertekschool.com>
2021-09-11 14:34:17 -0400

    Update CalculatorTests.java

--- src/test/java/com/cybertek/automation/tests/CalculatorTests.java
+++ src/test/java/com/cybertek/automation/tests/CalculatorTests.java
@@ -1,6 +1,10 @@
 package com.cybertek.automation.tests;
 
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.Test;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.remote.CapabilityType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class CalculatorTests {
@@ -9,5 +13,13 @@ public class CalculatorTests {
     public void test1(){
         //need to setup webdriver
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        //for consts: CapabilityType, MobileCapabilityType, AndroidMobileCapabilityType
+        //UiAutomator2 - used for android devices automation
+        desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+        desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.0");
+        desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
+        desiredCapabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, "com.android.calculator2");
+        desiredCapabilities.setCapability(AndroidMobileCapabilityType.APP_ACTIVITY, "com.android.calculator2.Calculator");
     }
 }
