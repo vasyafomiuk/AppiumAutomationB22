f6111f64f2ea3d5b34827448c4005ce42302440c
Vasyl Fomiuk <vasyl@cybertekschool.com>
2021-09-11 14:19:59 -0400

    first commit

---
+++ src/test/java/com/cybertek/automation/tests/CalculatorTests.java
@@ -0,0 +1,13 @@
+package com.cybertek.automation.tests;
+
+import org.junit.Test;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class CalculatorTests {
+
+    @Test
+    public void test1(){
+        //need to setup webdriver
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+    }
+}
