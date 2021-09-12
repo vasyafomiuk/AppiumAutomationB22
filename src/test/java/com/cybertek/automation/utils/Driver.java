616053f8bde37de5a8e48161ebb9a33d028a2392
Vasyl Fomiuk <vasyl@cybertekschool.com>
2021-09-12 14:48:21 -0400

    addede remote android driver

--- src/test/java/com/cybertek/automation/utils/Driver.java
+++ src/test/java/com/cybertek/automation/utils/Driver.java
@@ -19,19 +19,51 @@ public class Driver {
     }
 
     public static AppiumDriver<MobileElement> getDriver() {
+        String platform = ConfigurationReader.getProperty("platform");
         if (Objects.isNull(driver)) {
-            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-            desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
-            desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
-            desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.0");
-            desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
-            desiredCapabilities.setCapability(MobileCapabilityType.APP, "https://cybertek-appium.s3.amazonaws.com/etsy.apk");
-            try {
-                url = new URL("http://localhost:4723/wd/hub");
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
+            switch (platform) {
+                case "android":
+                    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                    desiredCapabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+                    desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, Platform.ANDROID);
+                    desiredCapabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "7.0");
+                    desiredCapabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel_2");
+                    desiredCapabilities.setCapability(MobileCapabilityType.APP, "https://cybertek-appium.s3.amazonaws.com/etsy.apk");
+                    try {
+                        url = new URL("http://localhost:4723/wd/hub");
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    driver = new AndroidDriver<>(url, desiredCapabilities);
+                    break;
+                case "android-remote":
+                    DesiredCapabilities caps = new DesiredCapabilities();
+
+                    // Set your access credentials
+                    caps.setCapability("browserstack.user", "testuser_1PhU8f");
+                    caps.setCapability("browserstack.key", "qxU7LUK78o8BK1ki799f");
+
+                    // Set URL of the application under test
+                    caps.setCapability("app", "bs://e0ce6dfd61f8f7d9fd9c4fb11c746b65fd1d79f1");
+
+                    // Specify device and os_version for testing
+                    caps.setCapability("device", "Google Pixel 3");
+                    caps.setCapability("os_version", "9.0");
+
+                    // Set other BrowserStack capabilities
+                    caps.setCapability("project", "My test appium automation");
+                    caps.setCapability("build", "Java Android");
+                    caps.setCapability("name", "Regression");
+
+                    // Initialise the remote Webdriver using BrowserStack remote URL
+                    // and desired capabilities defined above
+                    try {
+                        driver = new AndroidDriver<>(new URL("http://hub.browserstack.com/wd/hub"), caps);
+                    } catch (MalformedURLException e) {
+                        e.printStackTrace();
+                    }
+                    break;
             }
-            driver = new AndroidDriver<>(url, desiredCapabilities);
         }
         return driver;
     }
