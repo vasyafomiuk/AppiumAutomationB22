bba798435d38bf3f7a6a4fbe678487ff52dec741
Vasyl Fomiuk <vasyl@cybertekschool.com>
2021-09-12 10:52:45 -0400

    Update EtsyTests.java

--- src/test/java/com/cybertek/automation/tests/EtsyTests.java
+++ src/test/java/com/cybertek/automation/tests/EtsyTests.java
@@ -1,6 +1,7 @@
 package com.cybertek.automation.tests;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.AndroidMobileCapabilityType;
@@ -8,12 +9,17 @@ import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.Platform;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 public class EtsyTests {
     AppiumDriver<MobileElement> driver;
@@ -32,18 +38,22 @@ public class EtsyTests {
 
     @Test
     public void test() throws InterruptedException {
+        WebDriverWait wait = new WebDriverWait(driver, 20);
+
         //click get started button
         Thread.sleep(3000);
         driver.findElementById("com.etsy.android:id/btn_link").click();
-        Thread.sleep(3000);
+        //wait up to 20 seconds until element shows up
+        wait.until(ExpectedConditions.presenceOfElementLocated(By.id("com.etsy.android:id/edit_username")));
         driver.findElementById("com.etsy.android:id/edit_username").sendKeys("areatha@uspeakw.com");
-        Thread.sleep(3000);
         driver.findElementById("com.etsy.android:id/edit_password").sendKeys("Cybertek2020");
-        Thread.sleep(3000);
         driver.findElementById("com.etsy.android:id/button_signin").click();
-        Thread.sleep(3000);
-        driver.findElementByAccessibilityId("Show Navigation Drawer").click();
-        Thread.sleep(3000);
+
+        //wait up to 20 seconds until element shows up
+        By searchBy = MobileBy.AccessibilityId("Show Navigation Drawer");
+        wait.until(ExpectedConditions.presenceOfElementLocated(searchBy));
+        driver.findElement(searchBy).click();
+
         Thread.sleep(3000);
         MobileElement searchInput = driver.findElementById("com.etsy.android:id/search_src_text");
 
@@ -53,10 +63,15 @@ public class EtsyTests {
 
         //for some reason search by whole string doesn't work
         //so as work around, I've used a loop
+
         for (int i = 0; i < text.length(); i++) {
             actions.sendKeys(searchInput, text.substring(i, i + 1)).perform();
         }
 
+        List<MobileElement> populatedResults = driver.findElementsById("com.etsy.android:id/query_text");
+        populatedResults.get(0).click(); // click on 1st populated result
+
+        Thread.sleep(3000);
     }
 
     @After
